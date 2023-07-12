70a1704e138108406cabc05e6f27795ce3ba6d60
Homero Gelves <homerogelves@gmail.com>
2023-07-12 13:56:12 -0300

    mock y cambio de tema

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { CartWidget } from './CartWidget'
+import '../index.css'
 
 export const NavBar = () => {
   return (
-    <div className='bg-slate-950 flex items-center justify-around'>
-        <img src="./logo.png" className='w-36 h-36' alt="" />
+    <div className='nav flex items-center justify-around'>
+        <img src="https://i.redd.it/dftozda294791.png" className='w-40 h-40' alt="" />
         <nav className='text-white'>
             <a href="#" className='px-3'>Productos</a>
             <a href="#" className='px-3'>Sobre Nosotros</a>
